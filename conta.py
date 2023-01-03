3057ccab1ce6cf26d3425b593ee980180ac2e30a
Marcos Alexandre <marcos.alexandre.onika.nastri@gmail.com>
2023-01-03 20:07:48 -0300

    Adicionando atributos

--- conta.py
+++ conta.py
@@ -5,3 +5,7 @@ class Conta:
     def __init__(self):
 
         print("Construindo objeto... {}".format(self))
+        self.numero = 123
+        self.titular = "Marcos"
+        self.saldo = 55.0
+        self.limite = 1000.0
